f5c8aff1e9b39eab4c8d3a8896b8fe52e32c52ed
vivek bisht <vivek.bisht@ftechiz.com>
2025-04-10 18:14:29 +0530

    initial commit Lucklore Packaged Drinking Water

--- lib/util/app_constants.dart
+++ lib/util/app_constants.dart
@@ -4,7 +4,7 @@ import 'package:sixam_mart/features/language/domain/models/language_model.dart';
 import 'package:sixam_mart/util/images.dart';
 
 class AppConstants {
-  static const String appName = '1 Pm';
+  static const String appName = 'Lucklore Packaged Drinking Water';
   static const double appVersion = 3.0; ///Flutter Version: 3.27.4
 
   static const String fontFamily = 'Roboto';
